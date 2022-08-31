cf2b40808936011dc8d0263f3b2460c8471e2d20
noaholatoye <noaholatoye@gmail.com>
2022-08-31 12:56:15 +0100

    Reinstall dependencies and changed routes structures

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// import React, { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./assets/css/style.css";
 import "./assets/css/instincthub.css";
 import Landing from "./Pages/Landing";
@@ -8,12 +8,12 @@ import Individual from "./Pages/Apps/Individual";
 function App() {
   return (
     <>
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Landing} />
-          <Route path="/individual" component={Individual} />
-        </Switch>
-      </Router>
+      <BrowserRouter>
+          <Routes>
+              <Route index element={<Landing />} />
+              <Route path="individual/" element={<Individual />} />
+          </Routes>
+      </BrowserRouter>
     </>
   );
 }
