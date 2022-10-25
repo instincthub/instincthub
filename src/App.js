87d9ce8168202894f3fe64b0741d540f1f9882b0
MicroHub69 <msodq2018@gmail.com>
2022-10-25 09:10:42 +0100

    Footer Adjust

--- src/App.js
+++ src/App.js
@@ -4,49 +4,48 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./assets/css/style.css";
 import "./assets/css/instincthub.css";
 import LandingPage from "./Pages/Landing";
-// import About from "./Pages/About";
-// import Contact from "./Pages/Contact";
-// import Policy from "./Pages/Policy";
-// import Career from "./Pages/Career";
-// import Pricing from "./Pages/Pricing";
-// import Payment from "./Pages//Payment";
-// import JobDetails from "./Pages/JobDetails";
-// import Blog from "./Pages/Blog/Blog";
-// import BlogDetails from "./Pages/Blog/BlogDetails";
-// import Individual from "./Pages/Apps/Individual";
-// import KidsCanCode from "./Pages/Apps/KidsCanCode";
-// import Enterprise from "./Pages/Apps/Enterprises";
-// import CourseCreation from "./Pages/Apps/CourseCreation";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
+import Policy from "./Pages/Policy";
+import Career from "./Pages/Career";
+import Pricing from "./Pages/Pricing";
+import Payment from "./Pages//Payment";
+import JobDetails from "./Pages/JobDetails";
+import Blog from "./Pages/Blog/Blog";
+import BlogDetails from "./Pages/Blog/BlogDetails";
+import Individual from "./Pages/Apps/Individual";
+import KidsCanCode from "./Pages/Apps/KidsCanCode";
+import Enterprise from "./Pages/Apps/Enterprises";
+import CourseCreation from "./Pages/Apps/CourseCreation";
 
 function App() {
   return (
-      <BrowserRouter basename="/instincthub">
-        <Routes>
-          <Route index element={<LandingPage />} />
+    <BrowserRouter basename="/instincthub">
+      <Routes>
+        <Route index element={<LandingPage />} />
 
-          {/* About */}
-          {/* <Route path="/about/" element={<About />} />
-          <Route path="/about/contact/" element={<Contact />} />
-          <Route path="/about/career/" element={<Career />} />
-          <Route path="/about/career/:slug/" element={<JobDetails />} /> */}
+        {/* About */}
+        <Route path="/about/" element={<About />} />
+        <Route path="/about/contact/" element={<Contact />} />
+        <Route path="/about/career/" element={<Career />} />
+        <Route path="/about/career/:slug/" element={<JobDetails />} />
 
-          {/* Products */}
-          {/* <Route path="/products/individual/" element={<Individual />} />
-          <Route path="/products/kidscancode/" element={<KidsCanCode />} />
-          <Route path="/products/enterprise/" element={<Enterprise />} />
-          <Route path="/products/course-creation/" element={<CourseCreation />} />
-          <Route path="/products/pricing/" element={<Pricing />} />
-          <Route path="/product/payment/" element={<Payment />} /> */}
+        {/* Products */}
+        <Route path="/products/individual/" element={<Individual />} />
+        <Route path="/products/kidscancode/" element={<KidsCanCode />} />
+        <Route path="/products/enterprise/" element={<Enterprise />} />
+        <Route path="/products/course-creation/" element={<CourseCreation />} />
+        <Route path="/products/pricing/" element={<Pricing />} />
+        <Route path="/product/payment/" element={<Payment />} />
 
-          {/* Policies */}
-          {/* <Route path="/policies/privacy-policy/" element={<Policy />} /> */}
-          
-          {/* Posts */}
-          {/* <Route path="/blog" element={<Blog />} />
-          <Route path="/blog/:slug/" element={<BlogDetails />} /> */}
-          
-        </Routes>
-      </BrowserRouter>
+        {/* Policies */}
+        <Route path="/policies/privacy-policy/" element={<Policy />} />
+
+        {/* Posts */}
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/blog/:slug/" element={<BlogDetails />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-// import Navbar from "../components/Nav4Landing";
-// import Grid from "../components/3DGrid";
+import Navbar from "../components/Nav4Landing";
+import Grid from "../components/3DGrid";
 
 const LandingPage = () => {
   return (
     <div>
-    <h1>Hello World</h1>
-      {/* <Navbar /> */}
-      {/* <Grid /> */}
+      {/* <h1>Hello World</h1> */}
+      <Navbar />
+      <Grid />
     </div>
   );
 };
